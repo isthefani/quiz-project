8fa9cccd25d7897a0edaabbe7a9757adc00d20ce
isthefani <isthefani.vit@hotmail.com>
2023-10-09 00:00:38 -0300

    new add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Welcome from "./components/Welcome";
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>FRONT-END QUIZ</h1>
+        <Welcome />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const welcome = () => {
+  return (
+    <div id='welcome'>
+        <h2>Seja bem-vindos</h2>
+        <p>Clique no botão abaixo para começar:</p>
+        <button>Iniciar</button>
+    </div>
+  )
+}
+
+export default welcome
