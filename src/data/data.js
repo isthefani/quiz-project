a18ce445c0dd4938e92a9581957e54fd03ab4c56
GabrielCvml <gabrielcavalcanteml14@gmail.com>
2023-10-12 21:55:42 -0300

    Data novas perguntas

--- src/data/data.js
+++ src/data/data.js
@@ -1,50 +1,50 @@
 const quiz = [
     {id: 1, perg: "O que é React?", alts: [
-        {ok: "Um framework javascript para desenvolvimento web, para o desenvolvimento de apps e sites"},
-        "Um feijão",
-        "Uma laranja",
+        {certo: "Um framework javascript para desenvolvimento web, para o desenvolvimento de apps e sites"},
+        "Uma linguagem de programação",
+        "Uma biblioteca JavaScript para construção de interfaces de usuário",
         "Um framework javascript para desenvolvimento web, para o desenvolvimento de apps e sites",
-        "Um animal"
+        "Um banco de dados relacional"
         ]
     },
-    {id: 2, perg: "O que é Javascript?", alts: [
-        {ok: "Uma linguagem de programação."},
-        "Uma linguagem de programação.",
-        "Um feijão",
-        "Uma laranja",
-        "Um animal"
+    {id: 2, perg: "Qual é o hook do React usado para adicionar estado a um componente funcional?", alts: [
+        {certo: "useState"},
+        "useContext",
+        "useState",
+        "useEffect",
+        "useReducer"
         ]
     },
-    {id: 3, perg: "O que é Algoritmo?", alts: [
-        {ok: "Sequencia de passos lógicos."},
-        "Um feijão",
-        "Uma laranja",
-        "Um animal",
-        "Sequencia de passos lógicos.",
+    {id: 3, perg: "Qual é a sintaxe correta para criar um novo componente React?", alts: [
+        {certo: "Todas as opções estão corretas"},
+        "class MyComponent extends React.Component {}",
+        "onst MyComponent = () => {}",
+        "function MyComponent() {}",
+        "Todas as opções estão corretas",
         ]
     },
-    {id: 4, perg: "O que é Java?", alts: [
-        {ok: "Uma linguagem de programação."},
-        "Um feijão",
-        "Uma linguagem de programação.",
-        "Uma laranja",
-        "Um animal"
+    {id: 4, perg: "O que é JSX em React?", alts: [
+        {certo: "Uma linguagem de marcação similar ao HTML utilizada para definir a estrutura do componente"},
+        "Um formato de arquivo específico para componentes React",
+        "Uma linguagem de programação para escrever lógica de negócio em React",
+        "Uma linguagem de marcação similar ao HTML utilizada para definir a estrutura do componente",
+        "Nenhuma das opções"
         ]
     },
-    {id: 5, perg: "O que é Front-end?", alts: [
-        {ok: "Parte visível e interativa da aplicação"},
-        "Parte visível e interativa da aplicação",
-        "Um feijão",
-        "Uma laranja",
-        "Um animal"
+    {id: 5, perg: "Qual é o ciclo de vida de um componente React? ", alts: [
+        {certo: "Montagem, Atualização e Desmontagem "},
+        "Construção, Renderização e Atualização",
+        "Montagem, Atualização e Desmontagem ",
+        "Inicialização, Execução e Finalização",
+        "Criação, Fatoração, Execução"
         ]
     },
-    {id: 6, perg: "O que é a Vida?", alts: [
-        {ok: "É uma maravilha"},
-        "É uma maravilha",
-        "Um feijão",
-        "Uma laranja",
-        "Um animal"
+    {id: 6, perg: "Qual método é utilizado para fazer uma requisição HTTP em uma aplicação React?", alts: [
+        {certo: "fetch"},
+        "fetch",
+        "axios",
+        "http",
+        "request"
         ]
     },
 ]
