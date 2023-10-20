85eadb38b3ec3520064202eca05e993e166a386a
edilsonjrcode <2022111510116@iesp.edu.br>
2023-10-19 21:52:41 -0300

    ajuste botao prox e ant

---
+++ src/data/data2.js
@@ -0,0 +1,54 @@
+const quiz = 
+   
+    [
+        {id: 1, perg: "O que significa a sigla HTML?", alts: [
+            {certo: "HyperText Markup Language"},
+            "HyperText Markup Languag",
+            "HyperText Management Language",
+            "Hypertext Transfer Markup Language",
+            "Hyperlink Text Manipulation Language"
+            ]
+        },
+        {id: 2, perg: "Qual é a tag HTML usada para criar uma lista ordenada?", alts: [
+            {certo: "<ol>"},
+            "<ul>",
+            "<il>",
+            "<ol>",
+            "<list>"
+            ]
+        },
+        {id: 3, perg: "Qual atributo HTML é usado para definir o texto alternativo de uma imagem?", alts: [
+            {certo: "alt"},
+            "alt",
+            "src",
+            "href",
+            "text",
+            ]
+        },
+        {id: 4, perg: "Qual elemento HTML é usado para criar uma lista não ordenada em um documento web?", alts: [
+            {certo: "<ul>"},
+            "<ol>",
+            "<li>",
+            "<ul>",
+            "<dl>"
+            ]
+        },
+        {id: 5, perg: "Qual elemento HTML é usado para criar um link para uma folha de estilo externa?", alts: [
+            {certo: "<link>"},
+            "<style>",
+            "<a>",
+            "<span>",
+            "<link>"
+            ]
+        },
+        {id: 6, perg: "Qual é o elemento HTML usado para criar uma lista de itens com marcadores personalizados?", alts: [
+            {certo: "<li>"},
+            "<li>",
+            "<ul>",
+            "<ol>",
+            "<customlist>"
+            ]
+        },
+    ]
+
+export default quiz;
